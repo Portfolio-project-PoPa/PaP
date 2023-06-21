c71060ed3d9fa672aadcaadd125513d0f4b92aa8
GigaOrts <ortsortskhoev95@gmail.com>
2023-06-21 14:46:54 +0300

    Update EnemyAI.cs
    Противник за нами бегает, если войти в радиус обзора (Или спровоцировать выстрелом, можно доделать когда будут выстрелы).
    Противник также атакует (Дебагом пока) Доделаем в стейтмашину

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,9 +9,10 @@ public class EnemyAI : MonoBehaviour
 {
     [SerializeField] private Transform _target;
     [SerializeField] private float _chaseRange;
+
     private NavMeshAgent _agent;
     private float _distanceToTarget = Mathf.Infinity;
-
+    private bool _isProvoked;
 
     void Start()
     {
@@ -20,9 +22,36 @@ public class EnemyAI : MonoBehaviour
     void Update()
     {
         _distanceToTarget = Vector3.Distance(_target.position, transform.position);
+        
+        if(_distanceToTarget <= _chaseRange)
+            _isProvoked = true;
 
-        if (_distanceToTarget <= _chaseRange)
-            _agent.SetDestination(_target.position);
+        if (_isProvoked)
+        {
+            EngageTarget();
+        }
+    }
+
+    private void EngageTarget()
+    {
+        if (_distanceToTarget > _agent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+        else
+        {
+            AttackTarget();
+        }
+    }
+
+    private void AttackTarget()
+    {
+        Debug.Log("Ударил");
+    }
+
+    private void ChaseTarget()
+    {
+        _agent.SetDestination(_target.position);
     }
 
     private void OnDrawGizmosSelected()
