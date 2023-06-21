4e06c7267eacc5fe4a1dbcd54c1cbb8fa2e133f4
coldborne <coldborneVV@gmail.com>
2023-06-22 00:45:14 +0300

    Скрипты стрельбы
    Добавил скрипт Shooter.cs для того, чтобы объект с данным компонентом мог стрелять пулями
    Также добавил скрипт Bullet для того, чтобы можно было описывать объекты типа Bullet и использовать их для стрельбы (И, возможно, для объектов в инвентаре)

---
+++ Assets/Scripts/Shooter/Bullet.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float speed = 20f;
+    [SerializeField] private float lifeDuration = 3f;
+
+    private float _lifeTimer;
+
+    private void OnEnable()
+    {
+        _lifeTimer = lifeDuration;
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * (speed * Time.deltaTime));
+        _lifeTimer -= Time.deltaTime;
+
+        // When the bullet's life time exceeds, put it back in the pool
+        if (_lifeTimer <= 0f)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
