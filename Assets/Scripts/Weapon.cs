9f06133bb89cb1014fd30ed0960d1332ed2ef725
GigaOrts <ortsortskhoev95@gmail.com>
2023-06-24 21:29:43 +0300

    Скрипт Weapon.cs Для стрельбы рейкастом

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,18 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] Camera _FPCamera;
+    [SerializeField] float _range;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        Physics.Raycast(_FPCamera.transform.position, _FPCamera.transform.forward, out RaycastHit hit, _range);
+        Debug.Log(hit.collider.name);
     }
 }
