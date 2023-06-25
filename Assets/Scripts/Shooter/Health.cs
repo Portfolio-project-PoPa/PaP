dc6cc334dbb38a5ac9e98ddc730aa80a4e7d17a8
coldborne <coldborneVV@gmail.com>
2023-06-25 21:34:44 +0300

    Изменения в Life.cs
    1) Изменил название Life.cs на Health.cs
    2) Добавил int поле, отвечающее за максимальное значение здоровья
    3) В TakeDamage убрал запись в лог
    4) Поле _health переименовал в _value
    5) Убрал ненужные юзинги

--- Assets/Scripts/Shooter/Health.cs
+++ Assets/Scripts/Shooter/Health.cs
@@ -1,12 +1,10 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Life : MonoBehaviour
+public class Health : MonoBehaviour
 {
-    [SerializeField] private int _health = 100;
-
+    [SerializeField] private int _value = 100;
+    private int _maxValue = 100;
+    
     private void OnCollisionEnter(Collision damagingObject)
     {
         if (damagingObject.collider.TryGetComponent(out Bullet bullet))
@@ -17,11 +15,9 @@ public class Life : MonoBehaviour
 
     private void TakeDamage(int damage)
     {
-        _health -= damage;
+        _value -= damage;
 
-        Debug.Log("GetDamage" + damage + " Have health " + _health);
-        
-        if (_health <= 0)
+        if (_value <= 0)
         {
             Die();
         }
