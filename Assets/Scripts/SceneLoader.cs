20b1a60be17d5e01c0df2ec16738ab8cc4030dff
GigaOrts <ortsortskhoev95@gmail.com>
2023-06-26 11:54:03 +0300

    Перезагрузка уровня
    фонт выдавал ошибку, вроде не выдает теперь

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
