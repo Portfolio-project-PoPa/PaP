5417daa30cf21df729d60d225514d3435d6b291b
GigaOrts <ortsortskhoev95@gmail.com>
2023-06-26 23:21:13 +0300

    WeaponZoom.cs update
    MouseSensitivety when zoomed

--- Assets/Scripts/WeaponZoom.cs
+++ Assets/Scripts/WeaponZoom.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class WeaponZoom : MonoBehaviour
 {
     [SerializeField] private float _zoomInFOV = 40f;
     [SerializeField] private float _zoomOutFOV = 60f;
+    [SerializeField] private float _zoomInSensitivity = 0.5f;
+    [SerializeField] private float _zoomOutSensitivity = 2f;
 
+    private RigidbodyFirstPersonController _rigidbodyFirstPersonController;
     private Camera _mainCamera;
 
     private void Start()
     {
+        _rigidbodyFirstPersonController = GetComponent<RigidbodyFirstPersonController>();
         _mainCamera = Camera.main;
     }
 
@@ -16,11 +21,19 @@ public class WeaponZoom : MonoBehaviour
     {
         if (Input.GetButton("Fire2"))
         {
+            SetMouseSensitivity(_zoomInSensitivity);
             _mainCamera.fieldOfView = _zoomInFOV;
         }
         else
         {
+            SetMouseSensitivity(_zoomOutSensitivity);
             _mainCamera.fieldOfView = _zoomOutFOV;
         }
+    }
+
+    private void SetMouseSensitivity(float sensitivity)
+    {
+        _rigidbodyFirstPersonController.mouseLook.XSensitivity = sensitivity;
+        _rigidbodyFirstPersonController.mouseLook.YSensitivity = sensitivity;
     }
 }
