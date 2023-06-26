4833fd7551886ca5dd0f2a7f30bcf93970c76b38
GigaOrts <ortsortskhoev95@gmail.com>
2023-06-26 23:24:09 +0300

    Update WeaponZoom.cs
    Исправил, теперь берем только нужную часть класса, отвечающую за мышь

--- Assets/Scripts/WeaponZoom.cs
+++ Assets/Scripts/WeaponZoom.cs
@@ -8,12 +8,12 @@ public class WeaponZoom : MonoBehaviour
     [SerializeField] private float _zoomInSensitivity = 0.5f;
     [SerializeField] private float _zoomOutSensitivity = 2f;
 
-    private RigidbodyFirstPersonController _rigidbodyFirstPersonController;
+    private MouseLook _mouseLook;
     private Camera _mainCamera;
 
     private void Start()
     {
-        _rigidbodyFirstPersonController = GetComponent<RigidbodyFirstPersonController>();
+        _mouseLook = GetComponent<RigidbodyFirstPersonController>().mouseLook;
         _mainCamera = Camera.main;
     }
 
@@ -33,7 +33,7 @@ public class WeaponZoom : MonoBehaviour
 
     private void SetMouseSensitivity(float sensitivity)
     {
-        _rigidbodyFirstPersonController.mouseLook.XSensitivity = sensitivity;
-        _rigidbodyFirstPersonController.mouseLook.YSensitivity = sensitivity;
+        _mouseLook.XSensitivity = sensitivity;
+        _mouseLook.YSensitivity = sensitivity;
     }
 }
