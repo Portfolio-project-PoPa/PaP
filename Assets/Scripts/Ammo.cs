75f66d3750180d2cc873b4fa2ba9ec786c9e0e2c
GigaOrts <ortsortskhoev95@gmail.com>
2023-06-27 05:45:03 +0300

    Weapon and Ammo finish
    подсоединил аммо игрока к оружию (Ammo это сущность магазина, если что подправим, пока по курсу иду)

--- Assets/Scripts/Ammo.cs
+++ Assets/Scripts/Ammo.cs
@@ -1,18 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Ammo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int _ammoAmount = 10;
+
+    public int GetCurrentAmount()
     {
-        
+        return _ammoAmount;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ReduceCurrentAmount()
     {
-        
+        _ammoAmount--;
     }
 }
