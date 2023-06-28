6984220217769830141cf81ad3f75ed4fb3e3a74
GigaOrts <ortsortskhoev95@gmail.com>
2023-06-28 23:54:47 +0300

    KeyInput
    При нажатии клавиш 1 2 3 меняется оружие

--- Assets/Scripts/WeaponSwitch.cs
+++ Assets/Scripts/WeaponSwitch.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class WeaponSwitch : MonoBehaviour
 {
@@ -8,6 +9,39 @@ public class WeaponSwitch : MonoBehaviour
     void Start()
     {
         SetWeaponActive();    
+    }
+
+    void Update()
+    {
+        int previousWeapon = _currenWeapon;
+        ProcessKeyInput();
+        //ProcessScrollWheel();
+
+        if(previousWeapon != _currenWeapon)
+        {
+            SetWeaponActive();
+        }
+    }
+
+    private void ProcessScrollWheel()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void ProcessKeyInput()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            _currenWeapon = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            _currenWeapon = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            _currenWeapon = 2;
+        }
     }
 
     private void SetWeaponActive()
@@ -29,8 +63,4 @@ public class WeaponSwitch : MonoBehaviour
         }
     }
 
-    void Update()
-    {
-        
-    }
 }
